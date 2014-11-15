a4d8c507f9cc8a00b4768bd768f1330505098ff7
Chris Shaughnessy <chris@webifieddesign.com>
2014-11-14 21:08:13 -0500

    first rev

--- Age of Laika/ViewController.swift
+++ Age of Laika/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    @IBOutlet weak var enterHumanYearsTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        let stringFromTextField = enterHumanYearsTextField.text.toInt()!
+        let dogYears = 7
+        
+        let humanToDogYearsInteger = stringFromTextField * dogYears
+        dogYearsLabel.hidden = false
+        dogYearsLabel.text = "\(humanToDogYearsInteger)" + " years old"
+
+        enterHumanYearsTextField.resignFirstResponder()
+    }
+    @IBAction func realDogYearsButtonPressed(sender: UIButton) {
+        let stringFromTextField = enterHumanYearsTextField.text
+        let doubleFromTextField = Double((stringFromTextField as NSString).doubleValue)
+        
+        var realDogYears:Double
+        
+        if doubleFromTextField > 2 {
+            realDogYears = (10.5 * 2) + (doubleFromTextField - 2) * 4
+        } else {
+            realDogYears = doubleFromTextField * 10.5
+        }
+        
+        dogYearsLabel.hidden = false
+        dogYearsLabel.text = "\(realDogYears) Real Human Years"
+        enterHumanYearsTextField.resignFirstResponder()
+    }
 
 }
 
